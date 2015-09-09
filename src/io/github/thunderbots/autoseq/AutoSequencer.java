4f6ed88ab1ed3860f43c5a286d29992fa04336bd
Pranav Mathur <pranavmathur99@gmail.com>
2015-09-09 14:17:04 -0700

    Edit AutoSequencer

--- src/io/github/thunderbots/autoseq/AutoSequencer.java
+++ src/io/github/thunderbots/autoseq/AutoSequencer.java
@@ -1,15 +1,26 @@
 package io.github.thunderbots.autoseq;
 
+import java.io.File;
+import java.io.PrintStream;
+
 import io.github.thunderbots.sdk.opmode.SimpleOpMode;
 
 public class AutoSequencer {
 	
 	private SimpleOpMode opmode;
-	private String filename;
+	private File outputFile;
+	private PrintStream outputStream;
 	
 	public AutoSequencer(SimpleOpMode opmode, String filename) {
 		this.opmode = opmode;
-		this.filename = filename;
+		this.setOutputFile(filename);
+		this.outputStream = new PrintStream(this.outputStream);
+	}
+	
+	private void setOutputFile(String filename) {
+		this.outputFile = null;
+		// TODO: implement this
+		throw new UnsupportedOperationException();
 	}
 
 }
