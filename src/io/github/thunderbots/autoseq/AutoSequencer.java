c190ca472e01695ded5850977623716eb870a966
Pranav Mathur <pranavmathur99@gmail.com>
2015-12-19 14:50:37 -0800

    Remove all AutoSequencer Code

--- src/io/github/thunderbots/autoseq/AutoSequencer.java
+++ src/io/github/thunderbots/autoseq/AutoSequencer.java
@@ -1,26 +1,9 @@
 package io.github.thunderbots.autoseq;
 
-import java.io.File;
-import java.io.PrintStream;
-
-import io.github.thunderbots.lightning.opmode.SimpleOpMode;
-
 public class AutoSequencer {
 	
-	private SimpleOpMode opmode;
-	private File outputFile;
-	private PrintStream outputStream;
-	
-	public AutoSequencer(SimpleOpMode opmode, String filename) {
-		this.opmode = opmode;
-		this.setOutputFile(filename);
-		this.outputStream = new PrintStream(this.outputStream);
-	}
-	
-	private void setOutputFile(String filename) {
-		this.outputFile = null;
-		// TODO: implement this
-		throw new UnsupportedOperationException();
+	public AutoSequencer() {
+		
 	}
 
 }
